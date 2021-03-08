409205c1e8cca9e2d03870cb4cd594bb5fc78810
febert82 <febert82@gmail.com>
2021-03-08 16:00:45 +0100

    Initial commit

---
+++ Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    public static BackgroundLoop instance;
+    
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
